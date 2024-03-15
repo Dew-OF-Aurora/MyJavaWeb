1a3917516cf94f5905db6ebcc78647eb56dc0583
AuroraCXLv <AuroraCXLv@axisoft.com>
2024-03-15 11:28:39 +0800

    3.15

--- TestNetworking/TcpFileTransmit/src/main/java/org/example/Main.java
+++ TestNetworking/TcpFileTransmit/src/main/java/org/example/Main.java
@@ -1,5 +1,40 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+package org.example;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.URL;
+
+public class Main {
+	public static void main(String[] args) {
+		String fileName = "tempText";
+		DataInputStream dataInputStream;
+		DataOutputStream outputStream;
+		URL url = ClassLoader.getSystemResource(fileName);
+		long length = new File(url.getPath()).length();
+		try (Socket client = new Socket();) {
+			SocketAddress address = new InetSocketAddress("localhost", 8000);
+			client.connect(address);
+
+			dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(url.getPath())));
+			outputStream = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+
+			outputStream.writeUTF(fileName);
+			outputStream.writeLong(length);
+
+			byte[] fileByte = new byte[1024];
+			int count = 0;
+			int temp;
+			while ((temp = dataInputStream.read(fileByte)) != -1) {
+				count++;
+				outputStream.write(fileByte, 0, temp);
+			}
+			System.out.println(count * 1024);
+			System.out.println(length);
+			outputStream.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- TestNetworking/TcpFileTransmit/src/test/java/org/example/ClientTest.java
+++ TestNetworking/TcpFileTransmit/src/test/java/org/example/ClientTest.java
@@ -1,4 +1,37 @@
-import static org.junit.Assert.*;
+package org.example;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ClientTest {
-  
+	public static void main(String[] args) {
+
+		DataInputStream dataInputStream;
+		DataOutputStream dataOutputStream;
+		try (ServerSocket serverSocket = new ServerSocket()) {
+			serverSocket.bind(new InetSocketAddress("localhost", 8000));
+			Socket accept = serverSocket.accept();
+			dataInputStream = new DataInputStream(new BufferedInputStream(accept.getInputStream()));
+
+			String fileName = dataInputStream.readUTF();
+			System.out.println(fileName);
+			long length = dataInputStream.readLong();
+			System.out.println(length);
+
+			dataOutputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("C:\\Users\\AuroraCXLv\\Desktop\\" + fileName + ".txt")));
+			byte[] bytes = new byte[1024];
+			int temp;
+			int count = 0;
+			while ((temp = dataInputStream.read(bytes)) != -1) {
+				count += temp;
+				dataOutputStream.write(bytes, 0, temp);
+			}
+			System.out.println(count);
+			dataOutputStream.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
